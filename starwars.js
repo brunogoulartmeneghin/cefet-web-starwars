52acdc6cf9d311127f1378995aed4407a95dc744
Aluno <aluno@decom-OptiPlex-780>
2017-04-25 12:17:55 -0300

    att

--- starwars.js
+++ starwars.js
@@ -3,3 +3,18 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+let filmes = document.getElementById('movies').firstChild;
+
+$.ajax({
+  	url: 'https://swapi.co/api/films',
+  	method: 'GET',      // opcional: 'GET' é o valor padrão
+	success: function(resposta) {
+    	  // veja a resposta no terminal
+    	filmes.innerHTML = resposta.results[1];
+    	for(let muv of resposta.results){
+    		console.dir(muv.opening_crawl);
+    		filmes.append(muv.);
+    	}
+	}
+});
